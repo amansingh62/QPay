01b13b2d6b5e410dac468e93cc54eb917dcdf6d8
Aman Singh <coolaman6299@gmail.com>
2026-07-14 23:49:45 +0530

    Added optional phone or email option during registration

--- backend/src/modules/auth/authController.ts
+++ backend/src/modules/auth/authController.ts
@@ -2,31 +2,58 @@ import type { Request, Response } from "express";
 import { prisma } from "../../../lib/prisma.js";
 import bcrypt from "bcrypt";
 import { setAuthCookies } from "../../utils/cookies.js";
-import { signAccessToken, signRefreshToken } from "../../utils/tokens.js";
+import {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} from "../../utils/tokens.js";
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { name, email, phone, password } = req.body;
 
-    if (!name || !name.trim()) {
-      return res.status(400).json({ message: "Invalid name" });
+    if (!name?.trim()) {
+      return res.status(400).json({
+        message: "Invalid name",
+      });
     }
 
-    if (!phone || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+    if (!password) {
+      return res.status(400).json({
+        message: "Password is required",
+      });
+    }
+
+    if (!email && !phone) {
+      return res.status(400).json({
+        message: "Email or phone number is required",
+      });
+    }
+
+    const whereConditions = [];
+
+    if (email) {
+      whereConditions.push({
+        email: email.trim().toLowerCase(),
+      });
+    }
+
+    if (phone) {
+      whereConditions.push({
+        phone: phone.trim(),
+      });
     }
 
     const existingUser = await prisma.user.findFirst({
       where: {
-        OR: [
-          { email },
-          { phone },
-        ],
+        OR: whereConditions,
       },
     });
 
     if (existingUser) {
-      return res.status(409).json({ message: "User already exists" });
+      return res.status(409).json({
+        message: "User already exists",
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
@@ -34,8 +61,8 @@ export const register = async (req: Request, res: Response) => {
     await prisma.user.create({
       data: {
         name: name.trim(),
-        email: email.trim().toLowerCase(),
-        phone: phone.trim(),
+        email: email?.trim().toLowerCase(),
+        phone: phone?.trim(),
         password: hashedPassword,
       },
     });
@@ -53,23 +80,21 @@ export const register = async (req: Request, res: Response) => {
 };
 
 export const login = async (req: Request, res: Response) => {
-   try {
-     const { email, phone, password } = req.body;
+  try {
+    const { email, phone, password } = req.body;
 
     const user = await prisma.user.findFirst({
-        where: {
-            OR: [
-                { email },
-                { phone },
-            ]
-        }
+      where: {
+        OR: [{ email }, { phone }],
+      },
     });
 
-    if(!user) return res.status(403).json({ message: "User don't exists" });
+    if (!user) return res.status(403).json({ message: "User don't exists" });
 
     const isValid = await bcrypt.compare(password, user.password);
 
-    if(!isValid) return res.status(400).json({ message: "Invalid Credentials" });
+    if (!isValid)
+      return res.status(400).json({ message: "Invalid Credentials" });
 
     const accessToken = signAccessToken(user.id);
     const refreshToken = signRefreshToken(user.id);
@@ -77,12 +102,95 @@ export const login = async (req: Request, res: Response) => {
     setAuthCookies(res, accessToken, refreshToken);
 
     return res.status(200).json({ message: "User logged in successfully" });
-   }
-   
-   catch (error) {
+  } catch (error) {
     console.error("Login error: ", error);
 
     return res.status(500).json({ message: "Internal Server Error" });
-   }
+  }
 };
 
+export const refresh = async (req: Request, res: Response) => {
+  try {
+    const oldRefreshToken = req.cookies.refreshToken;
+
+    if (!oldRefreshToken) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const payload = verifyRefreshToken(oldRefreshToken);
+
+    if (!payload) {
+      return res.status(401).json({
+        message: "Invalid or expired refresh token",
+      });
+    }
+
+    const storedToken = await prisma.refreshToken.findUnique({
+      where: {
+        token: oldRefreshToken,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!storedToken) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    await prisma.refreshToken.delete({
+      where: {
+        id: storedToken.id,
+      },
+    });
+
+    const accessToken = signAccessToken(storedToken.user.id);
+    const newRefreshToken = signRefreshToken(storedToken.user.id);
+
+    await prisma.refreshToken.create({
+      data: {
+        token: newRefreshToken,
+        userId: storedToken.user.id,
+        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      },
+    });
+
+    setAuthCookies(res, accessToken, newRefreshToken);
+
+    return res.status(200).json({
+      message: "Token refreshed successfully",
+    });
+  } catch (error) {
+    console.error("Refresh Token Error:", error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const me = async (req: Request, res: Response) => {
+  const userId = req.userId;
+
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      phone: true,
+      profilePicture: true,
+      createdAt: true,
+    },
+  });
+
+  return res.status(200).json({ message: "User fetched successfully", user });
+};
